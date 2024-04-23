210fdd8098c840e383a38a2d4fabb5e3e52d80ab
Tray-PC <patradyn@gmail.com>
2024-04-23 14:30:37 +0700

    fix feeture check user and rebase dev

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/userController');
 
 router.post("/login", UserController.login)
 router.post("/register", UserController.register)
+router.post("/check", UserController.checkUser)
 
 module.exports = router;
